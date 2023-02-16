a2bcdf34de1602a0a592377ebaddda5784dbf7c1
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2023-02-16 20:04:19 +0100

    Fixed strings in routes

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,14 @@ import {Router} from 'express'
 
 const router = Router()
 
-router.get('v1/user-forms', () => {})
-router.post('v1/user-forms', () => {})
-router.get('v1/user-forms/:id', () => {})
+router.get("/v1/user-forms", (req, res) => {
+  res.json({message: "user-forms"});
+})
+router.post("/v1/user-forms", () => {})
+router.get("/v1/user-forms/:id", () => {})
 
-router.get('v1/house-forms', () => {})
-router.post('v1/house-forms', () => {})
-router.get('v1/house-forms/:id', () => {})
+router.get("/v1/house-forms", () => {})
+router.post("/v1/house-forms", () => {})
+router.get("/v1/house-forms/:id", () => {})
 
 export default router
