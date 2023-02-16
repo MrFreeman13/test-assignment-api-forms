42b0d0f00eeab8b64f9cb8582abb4003c869438a
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2023-02-16 21:07:57 +0100

    Cleaned up server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,8 @@ import router from "./router"
 
 const app = express()
 
-
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-app.get('/', (req, res) => {
-  console.log('Hello from Express')
-  res.status(200)
-  res.json({message: 'hello'})
-})
-
 app.use('/api', router)
 
 export default app
